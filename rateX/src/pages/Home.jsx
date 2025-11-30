ccc926f9aa6bc2da1445b4a2b228faf78d7285eb
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2025-12-01 00:14:51 +0530

    Remove vertical dividers from Home component
    Removed unnecessary vertical lines for better layout.

--- rateX/src/pages/Home.jsx
+++ rateX/src/pages/Home.jsx
@@ -10,12 +10,6 @@ const Home = ({currency, setCurrency, coinData}) => {
 
   return (
     <div className='relative w-full'>
-
-      <div className="hidden lg:block absolute top-0 bottom-0 left-40 w-px bg-neutral-300 "></div>
-
-
-      <div className="hidden lg:block absolute top-0 bottom-0 right-40 w-px bg-neutral-300"></div>
-
       <main className="min-h-screen flex flex-col space-y-10">
         <div className="border-b border-neutral-300">
           <Navbar coinData={coinData} setCurrency={setCurrency} currency={currency} />
