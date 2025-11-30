30d554382c40e5866bb48722932b6c66809add3e
Amrut Deshpande <amrutdeshpande2004@gmail.com>
2025-11-30 23:56:30 +0530

    Update login API endpoint to production URL

--- rateX/src/pages/Login.jsx
+++ rateX/src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
         e.preventDefault()
         setLoading(true)
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/login/', {
+            const response = await fetch('https://ratexbackend.onrender.com/api/login/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
